555393a707c6734da6834c11d8d0566a1cbbb2c1
Santiago Nicolás Czop <czopsantiago@gmail.com>
2021-11-08 17:53:37 -0300

    [ADD] - Added log out button without functionality on drawer

--- fiubademy/lib/src/home.dart
+++ fiubademy/lib/src/home.dart
@@ -25,60 +25,70 @@ class _HomePageState extends State<HomePage> {
 
 Widget buildExpandableBody(BuildContext context) {
   return Container(
-    color: Colors.white,
-    child: ElevatedButton(
-      onPressed: () {
-        Provider.of<Auth>(context, listen:false).deleteToken();
-      },
-      child: Text('Erase Token'),)
-  );
+      color: Colors.white,
+      child: ElevatedButton(
+        onPressed: () {
+          Provider.of<Auth>(context, listen: false).deleteToken();
+        },
+        child: Text('Erase Token'),
+      ));
 }
 
 Widget buildDrawer(BuildContext context) {
   return Drawer(
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        const UserAccountsDrawerHeader(
-          accountName: Text('Santiago Czop'),
-          accountEmail: Text('sczop@fi.uba.ar')
-        ),
-        ListTile(
-          leading: Icon(Icons.account_circle),
-          title: Text('My Profile'),
-          onTap: () {
-            Navigator.pop(context);
-          },
+    child: Column(
+      children: [
+        Expanded(
+          child: ListView(
+            children: [
+              const UserAccountsDrawerHeader(
+                  accountName: Text('Santiago Czop'),
+                  accountEmail: Text('sczop@fi.uba.ar')),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('My Profile'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Divider(),
+              ListTile(
+                leading: Icon(Icons.school),
+                title: Text('My Courses'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.supervisor_account),
+                title: Text('My Collaborations'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.favorite),
+                title: Text('Favourites'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Divider(),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
         const Divider(),
         ListTile(
-          leading: Icon(Icons.school),
-          title: Text('My Courses'),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.supervisor_account),
-          title: Text('My Collaborations'),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.favorite),
-          title: Text('Favourites'),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        const Divider(),
-        ListTile(
-          leading: Icon(Icons.message),
-          title: Text('Messages'),
-          onTap: () {
-            Navigator.pop(context);
-          },
+          onTap: () {},
+          leading: Icon(Icons.logout, color: Colors.red[700]),
+          title: Text('Log Out', style: TextStyle(color: Colors.red[700])),
         ),
       ],
     ),
