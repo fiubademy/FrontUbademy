1713682687a17f2e7d02f2a4b8d6047c08bf2951
Santiago Nicolás Czop <czopsantiago@gmail.com>
2021-11-08 02:05:38 -0300

    [ADD] - Added SignUpForm

--- fiubademy/lib/src/signup.dart
+++ fiubademy/lib/src/signup.dart
@@ -8,9 +8,6 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  bool _passwordObscured = true;
-  bool _passwordConfirmationObscured = true;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,97 +19,113 @@ class _SignUpPageState extends State<SignUpPage> {
               children: [
                 const Image(image: AssetImage('images/ubademy.png')),
                 const SizedBox(height: 16.0),
-                Form(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              textInputAction: TextInputAction.next,
-                              decoration: const InputDecoration(
-                                labelText: 'Name',
-                                filled: true,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 16.0),
-                          Expanded(
-                            child: TextFormField(
-                              textInputAction: TextInputAction.next,
-                              decoration: const InputDecoration(
-                                labelText: 'Last Name',
-                                filled: true,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 16.0),
-                      TextFormField(
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          hintText: 'example@email.com',
-                          labelText: 'Email',
-                          filled: true,
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      TextFormField(
-                        textInputAction: TextInputAction.next,
-                        decoration: InputDecoration(
-                            labelText: 'Password',
-                            filled: true,
-                            suffixIcon: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _passwordObscured = !_passwordObscured;
-                                  });
-                                },
-                                icon: Icon(_passwordObscured
-                                    ? Icons.visibility_off
-                                    : Icons.visibility))),
-                        obscureText: _passwordObscured,
-                      ),
-                      const SizedBox(height: 16.0),
-                      TextFormField(
-                        textInputAction: TextInputAction.done,
-                        decoration: InputDecoration(
-                            labelText: 'Password Confirmation',
-                            filled: true,
-                            suffixIcon: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _passwordConfirmationObscured =
-                                        !_passwordConfirmationObscured;
-                                  });
-                                },
-                                icon: Icon(_passwordConfirmationObscured
-                                    ? Icons.visibility_off
-                                    : Icons.visibility))),
-                        obscureText: _passwordConfirmationObscured,
-                      ),
-                      const SizedBox(height: 16.0),
-                      Row(
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text('I already have an account'),
-                          ),
-                          const Spacer(),
-                          const ElevatedButton(
-                            onPressed: null,
-                            child: Text('Sign up'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  bool _passwordObscured = true;
+  bool _passwordConfirmationObscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
+                    filled: true,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: TextFormField(
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    labelText: 'Last Name',
+                    filled: true,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            textInputAction: TextInputAction.next,
+            decoration: const InputDecoration(
+              hintText: 'example@email.com',
+              labelText: 'Email',
+              filled: true,
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+                labelText: 'Password',
+                filled: true,
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _passwordObscured = !_passwordObscured;
+                      });
+                    },
+                    icon: Icon(_passwordObscured
+                        ? Icons.visibility_off
+                        : Icons.visibility))),
+            obscureText: _passwordObscured,
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+                labelText: 'Password Confirmation',
+                filled: true,
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _passwordConfirmationObscured =
+                            !_passwordConfirmationObscured;
+                      });
+                    },
+                    icon: Icon(_passwordConfirmationObscured
+                        ? Icons.visibility_off
+                        : Icons.visibility))),
+            obscureText: _passwordConfirmationObscured,
+          ),
+          const SizedBox(height: 16.0),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('I already have an account'),
+              ),
+              const Spacer(),
+              const ElevatedButton(
+                onPressed: null,
+                child: Text('Sign up'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
