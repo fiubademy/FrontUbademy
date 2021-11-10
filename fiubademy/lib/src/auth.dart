c01151111f386a6f703af161a6c4b80a279d3699
Santiago Nicolás Czop <czopsantiago@gmail.com>
2021-11-10 01:21:47 -0300

    [ADD] - Added async factory creator to auth

--- fiubademy/lib/src/auth.dart
+++ fiubademy/lib/src/auth.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Auth extends ChangeNotifier {
   String? userToken;
+
+  Auth._init();
+
+  static Future<Auth> init() async {
+    final storage = FlutterSecureStorage();
+    String? savedToken = await storage.read(key: 'userToken');
+    var auth = Auth._init();
+    auth.userToken = savedToken;
+    return auth;
+  }
 
   String? getToken() {
     return userToken;
