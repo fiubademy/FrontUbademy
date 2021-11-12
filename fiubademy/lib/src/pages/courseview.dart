db5d7387272e34b248ff90510922265947a1feab
Santiago Nicolás Czop <czopsantiago@gmail.com>
2021-11-11 23:47:39 -0300

    [UPDATE] - Updated CourseView Rating Indicator for CourseRating

--- fiubademy/lib/src/pages/courseview.dart
+++ fiubademy/lib/src/pages/courseview.dart
@@ -194,26 +194,7 @@ class CourseViewPage extends StatelessWidget {
     return [
       Text('Reviews and Ratings', style: Theme.of(context).textTheme.headline6),
       const SizedBox(height: 16.0),
-      Row(
-        children: [
-          RatingBarIndicator(
-            rating: 4.3,
-            itemSize: 24,
-            itemBuilder: (context, index) =>
-                Icon(Icons.star, color: Colors.amber),
-          ),
-          const SizedBox(width: 8.0),
-          Text(
-            '4.3',
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          const Spacer(),
-          Text(
-            '(37 reviews)',
-            style: Theme.of(context).textTheme.subtitle1,
-          )
-        ],
-      ),
+      CourseRating(),
       const SizedBox(height: 8.0),
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
