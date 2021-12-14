2066911d856f1195fcabcd0cf770d5bb3548be87
Santiago Nicolás Czop <czopsantiago@gmail.com>
2021-12-14 02:31:53 -0300

    [ADD] - Added firebase

--- fiubademy/lib/main.dart
+++ fiubademy/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:fiubademy/src/pages/login.dart';
 import 'package:fiubademy/src/pages/home.dart';
@@ -7,7 +9,11 @@ import 'package:fiubademy/src/services/auth.dart';
 import 'package:fiubademy/src/services/user.dart';
 import 'package:fiubademy/src/services/server.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FiubademyApp());
 }
 
